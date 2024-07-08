b3c5b9ca7759110e87b938f6a504aa64c604699c
lz20r <zhiyaonaiara@gmail.com>
2024-07-08 17:54:45 +0200

    feat: Agregar controlador para buscar videos de YouTube
    Este cambio agrega un nuevo controlador llamado "youtube" que permite buscar videos de YouTube. El controlador utiliza el módulo "@SumiFX/Scraper" para realizar la búsqueda y obtener información como el título, la duración, el autor y la URL de los videos encontrados. Luego, envía la miniatura y la información de los videos como mensaje al remitente. Esta adición mejora la funcionalidad del sistema al permitir a los usuarios buscar y obtener información de videos de YouTube directamente desde el bot.

---
+++ src/plugins/information/youtube.js
@@ -0,0 +1,75 @@
+const Scraper = require("@SumiFX/Scraper"); 
+
+module.exports = {
+  name: "youtube",
+  aliases: ["yts", "ytsearch"],
+  category: "multimedia",
+  subcategory: "downloads",
+  description: "Busca videos de YouTube.",
+  usage: "youtube <titulo>",
+  botPermissions: ["SEND_MESSAGES", "ATTACH_FILES"],
+  userPermissions: [],
+  cooldown: 10,
+
+  async execute(sock, msg, args) { 
+    const from = msg.messages[0]?.key?.remoteJid;
+
+    if (!from) {
+      console.error("msg.messages[0].key.remoteJid no está definido.");
+      return;
+    }
+
+    if (!args.length) {
+      return sock.sendMessage(
+        from,
+        {
+          text:
+            "🍭 Ingresa el título de un video o canción de YouTube.\n\nEjemplo:\n> *" +
+            `${this.aliases[0]}* Gemini Aaliyah - If Only`,
+        },
+        { quoted: msg.messages[0] }
+      );
+    }
+ 
+    
+
+    const text = args.join(" ");
+    let results;
+    try {
+      results = await Scraper.ytsearch(text);
+    } catch (error) {
+      console.error("Error buscando en YouTube:", error);
+      return sock.sendMessage(
+        from,
+        { text: "❌ Error al buscar en YouTube." },
+        { quoted: msg.messages[0] }
+      );
+    }
+
+    if (!results || results.length === 0) {
+      return sock.sendMessage(
+        from,
+        { text: "❌ No se encontraron resultados." },
+        { quoted: msg.messages[0] }
+      );
+    }
+
+    let img = results[0].thumbnail;
+    let txt = `╭─⬣「 *YouTube Search* 」⬣\n`;
+    results.forEach((video, index) => {
+      txt += ` │  ≡◦ *🐢 Nro ∙* ${index + 1}\n`;
+      txt += ` │  ≡◦ *🍭 Titulo ∙* ${video.title}\n`;
+      txt += ` │  ≡◦ *🕜 Duración ∙* ${video.duration}\n`;
+      txt += ` │  ≡◦ *🪴 Publicado ∙* ${video.published}\n`;
+      txt += ` │  ≡◦ *📚 Autor ∙* ${video.author}\n`;
+      txt += ` │  ≡◦ *⛓ Url ∙* ${video.url}\n`;
+      txt += ` ╰──────────⬣\n\n`;
+    });
+
+    await sock.sendMessage(
+      from,
+      { image: { url: img }, caption: txt },
+      { quoted: msg.messages[0] }
+    );
+  },
+};

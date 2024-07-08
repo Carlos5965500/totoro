e5c4d1c77846677be6dda0c5f8a5e751741bf0ae
lz20r <zhiyaonaiara@gmail.com>
2024-07-08 17:53:55 +0200

    feat: Agregar controlador para descargar archivos de Mediafire
    Este cambio agrega un nuevo controlador llamado "mediafire" que permite descargar archivos de Mediafire. El controlador utiliza el módulo "scrapeMediaFire" para extraer información de la URL de Mediafire, como el nombre del archivo, la fecha de carga, el tipo de archivo y el tamaño. Luego, utiliza Axios para descargar el archivo y lo envía como documento adjunto en un mensaje. Esta adición mejora la funcionalidad del sistema al permitir a los usuarios descargar archivos de Mediafire directamente desde el bot.

---
+++ src/plugins/downloads/mediafire.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const { URL } = require('url');
+const { scrapeMediafire } = require('../../modules/scrapeMediaFire');
+const axios = require('axios');
+const path = require('path');
+const mime = require('mime-types');
+
+module.exports = {
+    name: "mediafire",
+    category: "downloads",
+    subcategory: "mediafire",
+    usage: "mediafire <enlace>",
+    description: "Descarga archivos de Mediafire",
+    async execute(sock, msg, args, text) {
+        if (!args[0]) {
+            return msg.reply(
+                "🍭 Ingresa el enlace del archivo de Mediafire junto al comando.\n\n" +
+                `Ejemplo: ${sock.prefix}mediafire https://www.mediafire.com/file/433hbpsc95unywu/Oshi_no_Ko_01.mp4/file?dkey=jpivv6z5osa&r=1587`
+            );
+        }
+
+        // Verificar la estructura del objeto msg
+        if (!msg.messages || !msg.messages[0] || !msg.messages[0].key || !msg.messages[0].key.remoteJid) {
+            return msg.reply("🍭 Error: Mensaje inválido. No se pudo determinar el remitente.");
+        }
+
+        try {
+            const url = args[0];
+
+            // Enviar reacción de carga al iniciar la descarga
+            await sock.sendMessage(msg.messages[0].key.remoteJid, {
+                react: {
+                    text: '⏳', // Emoji de carga
+                    key: msg.messages[0].key
+                }
+            });
+
+            const datos = await scrapeMediafire(url); 
+            const { title, size, uploadDate, dl_url, ext } = datos[1];
+            if (datos[0]) {
+                // Enviar reacción de error con signo de interrogación y cruz
+                await sock.sendMessage(msg.messages[0].key.remoteJid, {
+                    react: {
+                        text: '❓', // Emoji de interrogación y cruz
+                        key: msg.messages[0].key
+                    }
+                });
+                return msg.reply(`╭─⬣「 *Mediafire Download Error* 」⬣\n╰─ ≡◦ *🍭 Totoro no pudo encontrar el archivo solicitado. ¿URL Válida?*`);
+            }
+
+            const { data: fileBuffer } = await descargarArchivo(dl_url);
+            
+            const fileName = `${title}.${ext}`;
+            const mimetype = mime.lookup(fileName);
+
+            const messageOptions = {
+                document: fileBuffer,
+                mimetype: mimetype,
+                fileName: fileName,
+                caption: `╭─⬣「 *Mediafire Download* 」─⬣\n│ ≡◦ *🍭 fecha ∙* ${uploadDate}\n│ ≡◦ *🍭 Nombre ∙* ${title}\n│ ≡◦ *📚 Extensión ∙* ${ext}\n│ ≡◦ *⚖ Peso ∙* ${size}\n╰─⬣`
+            };
+
+            await sock.sendMessage(msg.messages[0].key.remoteJid, messageOptions);
+
+            // Enviar reacción de éxito al terminar la descarga
+            await sock.sendMessage(msg.messages[0].key.remoteJid, {
+                react: {
+                    text: '🍭', // Emoji de éxito
+                    key: msg.messages[0].key
+                }
+            });
+
+        } catch (error) {
+            console.error(error);
+            await sock.sendMessage(msg.messages[0].key.remoteJid, {
+                react: {
+                    text: '❌', // Emoji de error
+                    key: msg.messages[0].key
+                }
+            });
+            return msg.reply(`╭─⬣「 *Mediafire Download Error* 」⬣\n╰─ ≡◦ *🍭 Totoro está experimentando un error*\n> *Error*: ${error.message}`);
+        }
+    },
+};
+
+async function descargarArchivo(url) {
+    try {
+        // Configura Axios para obtener una respuesta tipo arraybuffer
+        const respuesta = await axios({
+            url,
+            method: 'GET',
+            responseType: 'arraybuffer'
+        });
+
+        // Extrae el nombre del archivo de la URL
+        const nombreArchivo = path.basename(new URL(url).pathname);
+
+        // Devuelve el buffer de datos y el nombre del archivo
+        return { data: respuesta.data, nombreArchivo };
+    } catch (error) {
+        console.error('Error al descargar el archivo:', error);
+        throw error; // Lanzar el error para que sea capturado por el bloque catch en execute
+    }
+}

7e4d93770c7587492d1794c3ac25c332fe1e7a81
lz20r <zhiyaonaiara@gmail.com>
2024-07-08 17:53:09 +0200

    feat: Agregar modelo de usuario totoUser
    Este cambio agrega el modelo de usuario totoUser, que define la estructura de la tabla "totousers" en la base de datos. El modelo incluye campos como "phone", "name", "age", "country", "regTime", "registered" y "serialNumber", con sus respectivas validaciones y configuraciones. Además, se agrega una función adicional "getCountryFromPhone" para obtener el país a partir del número de teléfono. Este modelo mejora la organización y la reutilización del código al encapsular la lógica relacionada con los usuarios en una clase dedicada.

---
+++ src/models/totoUser.js
@@ -0,0 +1,87 @@
+const { DataTypes } = require("sequelize");
+const TotoDB = require("../libs/db/totoDB");
+const { parsePhoneNumberFromString } = require('libphonenumber-js');
+
+const phoneRegex = /^\+?\d{1,3}[-. ]?\(?\d{1,4}\)?[-. ]?\d{1,4}[-. ]?\d{1,4[-. ]?\d{1,4}$/;
+
+const tDB = new TotoDB();
+
+const totoUser = tDB.sequelize.define(
+  "totoUser",
+  { 
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true, 
+    }, 
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 100],
+      },
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 9,
+        max: 70,
+      },
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+    regTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      validate: {
+        isDate: true,
+      },
+    },
+    registered: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+      validate: {
+        isBoolean: true,
+      },
+    },
+    serialNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1, 100],
+        isAlphanumeric: true,
+        isLowercase: true,
+        notEmpty: true,
+        is: /^[a-f0-9]+$/i,
+        isHash: function (value) {
+          if (value.length !== 32) {
+            throw new Error(
+              `El serialNumber debe tener 32 caracteres, pero tiene ${value.length}`
+            );
+          }
+        },
+      },
+    } 
+  },
+  {
+    tableName: "totousers",
+    createdAt: "regTime", // Use regTime as createdAt
+    timestamps: true, // createdAt, updatedAt
+    updatedAt: true, // updatedAt
+    deletedAt: true, // deletedAt
+  }
+);
+
+totoUser.getCountryFromPhone = function (phone) {
+  const phoneNumber = parsePhoneNumberFromString(phone);
+  return phoneNumber.country;
+}
+
+module.exports = totoUser;

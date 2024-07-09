ca0b40e1b71b269d3f781cac7dc922f8ff4d58c2
lz20r <zhiyaonaiara@gmail.com>
2024-07-10 01:13:55 +0200

    feat: Agregar registro de llamadas en el archivo de registro de totoro

--- src/functions/countTotoUsers.js
+++ src/functions/countTotoUsers.js
@@ -1,11 +1,18 @@
 const totoUser = require("../models/totoUser");
-
+const totoroLog = require("../functions/totoroLog");
 async function countTotoUsers() {
   try {
     const count = await totoUser.count();
+    totoroLog.info(
+      "./logs/functions/countTotoUsers.log",
+      `Usuarios registrados: ${count}`
+    );
     return count;
-  } catch (error) {
-    console.error("Error al contar usuarios registrados:", error);
+  } catch (error) { 
+    totoroLog.error(
+      "./logs/functions/countTotoUsers.log",
+      `Error al contar usuarios: ${error}`
+    );
     throw error;
   }
 }

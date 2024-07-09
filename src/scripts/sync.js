e156d226351fa680ddcd8838f74a6caf23413d31
lz20r <zhiyaonaiara@gmail.com>
2024-07-10 01:16:06 +0200

    feat: Agregar sincronización con base de datos de respaldo
    Se ha agregado la funcionalidad de sincronización con una base de datos de respaldo en el archivo "sync.js". Esta funcionalidad permite establecer una conexión con la base de datos de respaldo y sincronizar la tabla "totoUser". Además, se registran mensajes de éxito y error en el archivo de registro. Esta mejora proporciona una forma más eficiente y efectiva de mantener una copia de respaldo de la base de datos principal.

--- src/scripts/sync.js
+++ src/scripts/sync.js
@@ -1,39 +1,83 @@
 const TotoDB = require("../libs/db/totoDB");
 const totoUser = require("../models/totoUser");
+const { Sequelize } = require('sequelize');
+const totoroLog = require('../functions/totoroLog');
 
 class totoDBSync {
   constructor() {
     this.tDB = new TotoDB();
-    this.totoUser = totoUser;
+
+    // Definir la base de datos de respaldo SQLite
+    this.backupDB = new Sequelize({
+      dialect: 'sqlite',
+      storage: process.env.BACKUP_STORAGE_PATH || './database/totoDB.sqlite',
+      logging: process.env.BACKUP_LOGGING === 'true' ? console.log : false,
+    });
+
+    // Definir el modelo en la base de datos de respaldo
+    this.backupTotoUser = this.backupDB.define('totoUser', totoUser.rawAttributes, totoUser.options);
   }
 
   async sync() {
     let syncMessage = `
-▣───────────────────────────────────────────────···
+▣─────────────────────────────────────────────────────────────────···
 │ Base de datos: ${this.tDB.sequelize.getDatabaseName()}
-│───────────────────────────────────────────────···
-│ ⏰  ${new Date().toLocaleString("es-ES", { timeZone: "America/Argentina/Buenos_Aires" })}`;
+│─────────────────────────────────────────────────────────────────···
+│ ⏰  ${new Date().toLocaleString("es-ES", { timeZone: "Europe/Madrid" })}`;
 
     try {
       await this.tDB.sequelize.authenticate();
       syncMessage += `
-│ 🚀  Conexión exitosa a la base de datos: ${this.tDB.sequelize.getDatabaseName()}`;
-      
-      await this.totoUser.sync();
+│ 🚀  Conexión exitosa a la base de datos principal: ${this.tDB.sequelize.getDatabaseName()}`;
+
+      await totoUser.sync({ force: false });
       syncMessage += `
-│ 🔄  Tabla sincronizada: ${this.totoUser.getTableName()}`;
+│ 🔄  Tabla sincronizada: ${totoUser.getTableName()}`;
 
       await this.tDB.sequelize.sync({ force: false });
       syncMessage += `
-│ ✅  Sincronización completada con éxito`;
+│ ✅  Sincronización completada con éxito en la base de datos principal`;
+      totoroLog.info('./logs/scripts/sync.log', 'Sincronización completada con éxito en la base de datos principal')
     } catch (error) {
       syncMessage += `
-│ ❌  Error durante la sincronización: ${error.message}`;
+│ ⚠️   No conectado a ${this.tDB.sequelize.getDatabaseName()}, usando totoDB.sqlite como respaldo.`;
+      totoroLog.error('./logs/scripts/sync.log', `Error en la conexión a la base de datos principal: ${error.message}`);
+      
+      try {
+        await this.backupDB.authenticate();
+        syncMessage += `
+│ 🚀  Conexión exitosa a la base de datos de respaldo: ${this.backupDB.options.storage}`;
+
+        await this.backupTotoUser.sync({ force: false });
+        syncMessage += `
+│ 🔄  Tabla sincronizada: ${this.backupTotoUser.getTableName()}`;
+
+        await this.backupDB.sync({ force: false });
+        syncMessage += `
+│ ✅  Sincronización completada con éxito en la base de datos de respaldo`;
+        totoroLog.info('./logs/scripts/sync.log', 'Sincronización completada con éxito en la base de datos de respaldo');
+      } catch (backupError) {
+        syncMessage += `
+│ ❌  No se pudo conectar a la base de datos de respaldo.`;
+        totoroLog.error(`Error en la conexión a la base de datos de respaldo: ${backupError.message}`);
+      }
     } finally {
       syncMessage += `
-▣───────────────────────────────────────────────···
+▣──────────────────────────────────────────────────────────────────────────···
 `;
-      await this.tDB.sequelize.close();
+      try {
+        await this.tDB.sequelize.close();
+      } catch (closeError) {
+        totoroLog.error('./logs/scripts/sync.log', `Error cerrando la base de datos principal: ${closeError.message}`);
+      }
+
+      try {
+        await this.backupDB.close();
+      } catch (backupCloseError) {
+        totoroLog.error('./logs/scripts/sync.log', `Error cerrando la base de datos de respaldo: ${backupCloseError.message}`);
+      }
+
+      totoroLog.info('./logs/scripts/sync.log', 'Proceso de sincronización terminado');
       console.log(syncMessage);
     }
   }

fd36ad0e74e040d85c7d41884f430705202b6d74
lz20r <zhiyaonaiara@gmail.com>
2024-07-10 01:14:29 +0200

    feat: Actualizar controlador "plugins" para utilizar la función totoroLog.info

--- src/libs/db/totoDB.js
+++ src/libs/db/totoDB.js
@@ -1,23 +1,37 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const totoroLog = require('../../functions/totoroLog');
 
-class TotoDB { 
+class TotoDB {
   constructor() {
     this.sequelize = new Sequelize(
-      process.env.TOTORO_DATABASE, // Database name
-      process.env.TOTORO_USERNAME, // Username
-      process.env.TOTORO_PASSWORD, // Password
+      process.env.TOTORO_DATABASE, // Nombre de la base de datos
+      process.env.TOTORO_USERNAME, // Nombre de usuario
+      process.env.TOTORO_PASSWORD, // Contraseña
       {
         host: process.env.TOTORO_HOST, // Host
-        port: process.env.TOTORO_PORT, // Port
-        dialect: 'mysql', // Specify the dialect here
+        port: process.env.TOTORO_PORT, // Puerto
+        dialect: 'mysql', // Especifica el dialecto aquí
         logging: `${process.env.TOTORO_LOGGING}` === 'true' ? console.log : false,
       }
     );
   }
 
-  isConnected() {
-    return this.sequelize.authenticate();
+  async isConnected() {
+    try {
+      await this.sequelize.authenticate();
+      totoroLog.info(
+        './logs/libs/totoDB.log',
+        `[${process.env.TOTORO_DATABASE}] Conectado a la base de datos.`
+      );
+      return true;
+    } catch (error) {
+      totoroLog.error(
+        './logs/libs/totoDB.log',
+        `[LIBS] ${process.env.TOTORO_DATABASE} Error al conectar a la base de datos: ${error.message}`
+      );
+      return false;
+    }
   }
 }
 

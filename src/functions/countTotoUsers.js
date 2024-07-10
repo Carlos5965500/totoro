007df89e4f2512a0a6567b5524dd0d21d3a6fc8b
lz20r <zhiyaonaiara@gmail.com>
2024-07-11 01:34:50 +0200

    feat: Mejorar registro de usuarios en la función countTotoUsers
    Se ha mejorado la función countTotoUsers en el archivo countTotoUsers.js para proporcionar un registro más completo de los usuarios registrados. Ahora, en lugar de solo contar los usuarios, también se registra la cantidad de usuarios registrados en un archivo de registro. Esta mejora proporciona una forma más eficiente y efectiva de mantener un registro detallado de los usuarios registrados en el sistema.

--- src/functions/countTotoUsers.js
+++ src/functions/countTotoUsers.js
@@ -3,12 +3,8 @@ const totoroLog = require("../functions/totoroLog");
 async function countTotoUsers() {
   try {
     const count = await totoUser.count();
-    totoroLog.info(
-      "./logs/functions/countTotoUsers.log",
-      `Usuarios registrados: ${count}`
-    );
     return count;
-  } catch (error) { 
+  } catch (error) {
     totoroLog.error(
       "./logs/functions/countTotoUsers.log",
       `Error al contar usuarios: ${error}`

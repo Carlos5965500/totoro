b29de7fa5f1ad9c070945f271a227170f7664ee4
lz20r <zhiyaonaiara@gmail.com>
2024-07-11 18:49:51 +0200

    feat: Mejorar lógica de visualización de archivos de logs
    Se ha mejorado la lógica de visualización de archivos de logs en el archivo logs.js. Ahora, si no hay archivos de logs generados, se muestra un mensaje indicando que no hay archivos para mostrar. Esta mejora proporciona una forma más eficiente y efectiva de manejar la visualización de archivos de logs en el sistema.

--- src/functions/totoroLog.js
+++ src/functions/totoroLog.js
@@ -1,47 +1,46 @@
-const winston = require('winston');
+const winston = require("winston");
+const path = require("path");
 
 const createLogger = (logFilePath) => {
-    return winston.createLogger({
-        format: winston.format.combine(
-            winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-            winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`)
-        ),
-        transports: [
-            new winston.transports.Console(),
-            new winston.transports.File({ filename: logFilePath })
-        ],
-    });
+  return winston.createLogger({
+    format: winston.format.combine(
+      winston.format.timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
+      winston.format.printf(
+        ({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`
+      )
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: logFilePath }),
+    ],
+  });
 };
 
 const createLogFunction = (logLevel) => {
-    return (logFilePath, message) => {
-        const logger = createLogger(logFilePath);
-        logger.log(logLevel, message);
-    };
+  return (logFilePath, message) => {
+    const logger = createLogger(logFilePath);
+    logger.log(logLevel, message);
+  };
 };
 
 const totoroLog = {
-    error: createLogFunction('error'),
-    success: createLogFunction('success'),
-    warn: createLogFunction('warn'),
-    info: createLogFunction('info'),
-    http: createLogFunction('http'),
-    verbose: createLogFunction('verbose'),
-    debug: createLogFunction('debug'),
-    silly: createLogFunction('silly'),
-    react: createLogFunction('react'),
-    rejectCallback: (logFilePath, error) => {
-        const logger = createLogger(logFilePath);
-        logger.error( 
-            `Error: ${error.message}`,
-            `Stack: ${error.stack}`
-        );
-    },
-
-    resolveCallback: (logFilePath, message) => {
-        const logger = createLogger(logFilePath);
-        logger.info(message);
-    } 
+  error: createLogFunction("error"),
+  success: createLogFunction("info"), // El nivel 'success' no existe en winston, usaremos 'info'
+  warn: createLogFunction("warn"),
+  info: createLogFunction("info"),
+  http: createLogFunction("http"),
+  verbose: createLogFunction("verbose"),
+  debug: createLogFunction("debug"),
+  silly: createLogFunction("silly"),
+  react: createLogFunction("info"), // El nivel 'react' no existe en winston, usaremos 'info'
+  rejectCallback: (logFilePath, error) => {
+    const logger = createLogger(logFilePath);
+    logger.error(`Error: ${error.message}\nStack: ${error.stack}`);
+  },
+  resolveCallback: (logFilePath, message) => {
+    const logger = createLogger(logFilePath);
+    logger.info(message);
+  },
 };
 
 module.exports = totoroLog;

7274fe5559330990c4342b76d2d06fb3d1b9e92b
lz20r <zhiyaonaiara@gmail.com>
2024-07-11 18:49:39 +0200

    feat: Mejorar lógica de visualización de archivos de logs

--- src/events/call.js
+++ src/events/call.js
@@ -3,19 +3,19 @@ const totoroLog = require("../functions/totoroLog");
 
 module.exports = {
   name: "call",
-  
-  async load(call, sock) {
+
+  async load(call, totoro) {
     const { id, from } = call[0];
 
-    await sock.rejectCall(id, from);
+    await totoro.rejectCall(id, from);
 
-    getHistoryMsg(sock, from, 1).then((msg) => {
+    getHistoryMsg(totoro, from, 1).then((msg) => {
       const { messages } = msg;
 
       if (messages[0].messageStubType === "REVOKE") return;
 
-      sock.sendMessage(from, {
-        text: "No puedo recibir llamadas.",
+      totoro.sendMessage(from, {
+        text: "📞 Lamentablemente no puedo recibir llamadas.",
       });
     });
 

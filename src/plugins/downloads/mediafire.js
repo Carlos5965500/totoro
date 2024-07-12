ea58c5e8dc781d20eeda2bdc56436ae6568e799b
lz20r <zhiyaonaiara@gmail.com>
2024-07-12 15:59:19 +0200

    Actualizar función execute en messageUtils.js

--- src/plugins/downloads/mediafire.js
+++ src/plugins/downloads/mediafire.js
@@ -1,113 +1,125 @@
-const fs = require('fs');
-const { URL } = require('url');
-const { scrapeMediafire } = require('../../modules/scrapeMediaFire');
-const axios = require('axios');
-const path = require('path');
-const mime = require('mime-types');
+const { scrapeMediafire } = require("../../modules/scrapeMediaFire");
+const verifyUser = require("../../utils/verifyuser");
 const totoroLog = require("../../functions/totoroLog");
-const sendError = require("../../functions/messages");
-
+const { sendWarning, sendError, help } = require("../../functions/messages");
+const axios = require("axios");
+const AdmZip = require("adm-zip");
 
 module.exports = {
-    name: "mediafire",
-    category: "downloads",
-    subcategory: "mediafire",
-    usage: "mediafire <enlace>",
-    description: "Descarga archivos de Mediafire",
-    async execute(totoro, msg, args, text) {
-        if (!args[0]) {
-            return msg.reply(
-                "🍭 Ingresa el enlace del archivo de Mediafire junto al comando.\n\n" +
-                `Ejemplo: ${totoro.prefix}mediafire https://www.mediafire.com/file/433hbpsc95unywu/Oshi_no_Ko_01.mp4/file?dkey=jpivv6z5osa&r=1587`
-            );
-        }
+  name: "mediafire",
+  aliases: ["mf"],
+  category: "multimedia",
+  subcategory: "downloads",
+  description: "Descarga archivos de Mediafire.",
+  usage: "mediafire <mediafire url>",
+  botPermissions: ["SEND_MESSAGES", "ATTACH_FILES"],
+  userPermissions: [],
+  cooldown: 10,
 
-        // Verificar la estructura del objeto msg
-        if (!msg.messages || !msg.messages[0] || !msg.messages[0].key || !msg.messages[0].key.remoteJid) {
-            return msg.reply("🍭 Error: Mensaje inválido. No se pudo determinar el remitente.");
-        }
+  async execute(totoro, msg, args) {
+    const participant = msg.messages?.[0]?.key?.participant;
+    const remoteJid = msg.messages?.[0]?.key?.remoteJid;
 
-        try {
-            const url = args[0];
-
-            // Enviar reacción de carga al iniciar la descarga
-            await totoro.sendMessage(msg.messages[0].key.remoteJid, {
-                react: {
-                    text: '⏳', // Emoji de carga
-                    key: msg.messages[0].key
-                }
-            });
-
-            const datos = await scrapeMediafire(url); 
-            const { title, size, uploadDate, dl_url, ext } = datos[1];
-            if (datos[0]) {
-                // Enviar reacción de error con signo de interrogación y cruz
-                await totoro.sendMessage(msg.messages[0].key.remoteJid, {
-                    react: {
-                        text: '❓', // Emoji de interrogación y cruz
-                        key: msg.messages[0].key
-                    }
-                });
-                return msg.reply(`╭─⬣「 *Mediafire Download Error* 」⬣\n╰─ ≡◦ *🍭 Totoro no pudo encontrar el archivo solicitado. ¿URL Válida?*`);
-            }
-
-            const { data: fileBuffer } = await descargarArchivo(dl_url);
-            
-            const fileName = `${title}.${ext}`;
-            const mimetype = mime.lookup(fileName);
-
-            const messageOptions = {
-                document: fileBuffer,
-                mimetype: mimetype,
-                fileName: fileName,
-                caption: `╭─⬣「 *Mediafire Download* 」─⬣\n│ ≡◦ *🍭 fecha ∙* ${uploadDate}\n│ ≡◦ *🍭 Nombre ∙* ${title}\n│ ≡◦ *📚 Extensión ∙* ${ext}\n│ ≡◦ *⚖ Peso ∙* ${size}\n╰─⬣`
-            };
-
-            await totoro.sendMessage(msg.messages[0].key.remoteJid, messageOptions);
-
-            // Enviar reacción de éxito al terminar la descarga
-            await totoro.sendMessage(msg.messages[0].key.remoteJid, {
-                react: {
-                    text: '🍭', // Emoji de éxito
-                    key: msg.messages[0].key
-                }
-            });
-
-        } catch (error) {
-            totoroLog.error(
-                './logs/plugins/downloads/mediafire.log',
-                `Error al descargar el archivo: ${error.message}`
-            );
-            await totoro.sendMessage(msg.messages[0].key.remoteJid, {
-                react: {
-                    text: '❌', // Emoji de error
-                    key: msg.messages[0].key
-                }
-            });
-            return msg.reply(`╭─⬣「 *Mediafire Download Error* 」⬣\n╰─ ≡◦ *🍭 Totoro está experimentando un error*\n> *Error*: ${error.message}`);
-        }
-    },
-};
-
-async function descargarArchivo(url) {
-    try {
-        // Configura Axios para obtener una respuesta tipo arraybuffer
-        const respuesta = await axios({
-            url,
-            method: 'GET',
-            responseType: 'arraybuffer'
-        });
-
-        // Extrae el nombre del archivo de la URL
-        const nombreArchivo = path.basename(new URL(url).pathname);
-
-        // Devuelve el buffer de datos y el nombre del archivo
-        return { data: respuesta.data, nombreArchivo };
-    } catch (error) {
-        totoroLog.error(
-            './logs/plugins/downloads/mediafire.log',
-            `Error al descargar el archivo: ${error.message}`
-        );
-        sendError(totoro, msg,  `Error al descargar el archivo: ${error.message}`);
+    if (!participant && !remoteJid) {
+      return sendError(
+        totoro,
+        msg,
+        "No se pudo obtener el número del usuario o el chat."
+      );
     }
-}
+
+    if (!args[0] || !args[0].match(/mediafire/gi)) {
+      return help(
+        totoro,
+        msg,
+        "mediafire",
+        "Descarga archivos de Mediafire.",
+        "mediafire <mediafire url>"
+      );
+    }
+
+    let user;
+    try {
+      user = await verifyUser(participant || remoteJid);
+    } catch (error) {
+      if (
+        error.message ===
+        "No estás registrado. Por favor, regístrate antes de usar este comando."
+      ) {
+        return sendWarning(totoro, msg, error.message);
+      } else {
+        return sendError(totoro, msg, error.message);
+      }
+    }
+
+    try {
+      const [error, { title, uploadDate, ext, size, dl_url }] =
+        await scrapeMediafire(args[0]);
+      if (error) {
+        return sendError(
+          totoro,
+          msg,
+          "Hubo un error al intentar obtener los detalles del archivo de Mediafire."
+        );
+      }
+
+      if (size.includes("GB") || parseFloat(size.replace(" MB", "")) > 400) {
+        return sendWarning(
+          totoro,
+          msg,
+          "El archivo pesa más de 400 MB, se canceló la descarga."
+        );
+      }
+
+      await msg.react("🔍"); // Reacción al iniciar la descarga
+
+      const response = await axios({
+        url: dl_url,
+        method: "GET",
+        responseType: "arraybuffer",
+      });
+
+      const zip = new AdmZip();
+      zip.addFile(`${title}.${ext}`, Buffer.from(response.data));
+      const zipBuffer = zip.toBuffer();
+
+      // Verificación del tamaño del archivo zip para que no exceda los 2 GB
+      if (zipBuffer.length > 2 * 1024 * 1024 * 1024) {
+        return sendWarning(
+          totoro,
+          msg,
+          "El archivo comprimido pesa más de 2 GB, se canceló la descarga."
+        );
+      }
+
+      let txt = `╭─⬣「 *Mediafire Download* 」⬣\n`;
+      txt += `│  ≡◦ *🍭 Título ∙* ${title}\n`;
+      txt += `│  ≡◦ *🪴 Subido el ∙* ${uploadDate}\n`;
+      txt += `│  ≡◦ *⚖ Peso ∙* ${size}\n`;
+      txt += `╰─⬣`;
+
+      await totoro.sendMessage(
+        remoteJid || participant,
+        {
+          document: zipBuffer,
+          mimetype: "application/zip",
+          fileName: `${title}.zip`,
+          caption: txt,
+        },
+        { quoted: msg.messages[0], asDocument: user.useDocument }
+      );
+
+      await msg.react("🍭"); // Reacción al finalizar la descarga
+    } catch (error) {
+      totoroLog.error(
+        "./logs/mediafire.log",
+        `Error al descargar archivo de Mediafire: ${error}`
+      );
+      await sendError(
+        totoro,
+        msg,
+        "Hubo un error al intentar descargar el archivo."
+      );
+    }
+  },
+};

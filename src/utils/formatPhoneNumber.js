be47a92ab970ce234ca9d28b5035ab10e6ac225d
lz20r <zhiyaonaiara@gmail.com>
2024-07-13 03:18:38 +0200

    feat: Agregar función para formatear números de teléfono

---
+++ src/utils/formatPhoneNumber.js
@@ -0,0 +1,13 @@
+// utils/formatPhoneNumber.js
+const { parsePhoneNumberFromString } = require("libphonenumber-js");
+
+module.exports = function formatPhoneNumber(phone) {
+  const phoneNumber = parsePhoneNumberFromString(phone);
+
+  if (phoneNumber) {
+    // Formatear el número de teléfono en formato internacional
+    return phoneNumber.formatInternational();
+  }
+
+  return phone; // Devuelve el número original si no se puede formatear
+};

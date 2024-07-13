dafdb142bf1541051eb440d12f67540436d30683
lz20r <zhiyaonaiara@gmail.com>
2024-07-13 03:18:43 +0200

    feat: Agregar manejo de errores en la función verifyUser
    Se ha agregado manejo de errores en la función verifyUser del archivo verifyuser.js. Ahora, en caso de que la base de datos de usuarios no esté definida, se enviará un mensaje de error al usuario. Además, se han agregado mensajes de error para informar al usuario si no está registrado o no tiene una cuenta premium al intentar verificar su usuario.

--- src/utils/verifyuser.js
+++ src/utils/verifyuser.js
@@ -1,19 +1,21 @@
 const totoUser = require("../models/totoUser");
 const totoroLog = require("../functions/totoroLog");
+const { sendError } = require("../functions/messages");
+
 async function verifyUser(participant) {
   if (!totoUser) {
     totoroLog.error(
       "./logs/verifyUser.log",
       "La base de datos de usuarios no está definida."
     );
-    throw new Error("La base de datos de usuarios no está definida.");
+    sendError(
+      totoro,
+      msg,
+      "No se pudo verificar tu usuario. Por favor, intenta de nuevo más tarde."
+    );
   }
 
   const phone = participant.split("@")[0];
-  totoroLog.info(
-    "./logs/verifyUser.log",
-    `Buscando usuario con phone: ${phone}`
-  );
 
   const user = await totoUser.findOne({ where: { phone: phone } });
   if (!user) {
@@ -21,8 +23,28 @@ async function verifyUser(participant) {
       "./logs/verifyUser.log",
       "Usuario no encontrado en la base de datos."
     );
-    throw new Error(
+    sendError(
+      totoro,
+      msg,
       "No estás registrado. Por favor, regístrate antes de usar este comando."
+    );
+  }
+
+  if (!user.registered) {
+    totoroLog.warn("./logs/verifyUser.log", "Usuario no está registrado.");
+    sendError(
+      totoro,
+      msg,
+      "No estás registrado. Por favor, regístrate antes de usar este comando."
+    );
+  }
+
+  if (!user.premium) {
+    totoroLog.warn("./logs/verifyUser.log", "Usuario no es premium.");
+    sendError(
+      totoro,
+      msg,
+      "No tienes acceso a esta función. Por favor, adquiere una suscripción premium para usarla."
     );
   }
 

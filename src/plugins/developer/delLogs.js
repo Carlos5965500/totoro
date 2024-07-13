eaa5d453050a30a16a2c8ee563ddcbeab9b25ae7
lz20r <zhiyaonaiara@gmail.com>
2024-07-13 03:18:09 +0200

    feat: Agregar función para eliminar archivos y carpetas de logs
    Se ha agregado una nueva función en el archivo delLogs.js que permite eliminar todos los archivos y carpetas de logs generados en la carpeta "logs". Esto se hace mediante el comando "deletelogs". Además, se han añadido mensajes de éxito y error para informar al usuario sobre el resultado de la operación.

--- src/plugins/developer/delLogs.js
+++ src/plugins/developer/delLogs.js
@@ -3,13 +3,16 @@ const path = require("path");
 const fs = require("fs");
 const totoroLog = require("../../functions/totoroLog");
 const { sendError } = require("../../functions/messages");
+const { dev } = require("./bash");
 
 module.exports = {
   name: "deletelogs",
   category: "developer",
   subcategory: "owner",
   usage: "<deletelogs>",
-  description: "Elimina todos los archivos de logs generados en la carpeta logs",
+  description:
+    "Elimina todos los archivos de logs generados en la carpeta logs",
+  dev: dev,
 
   async execute(totoro, msg, _) {
     try {
@@ -40,40 +43,49 @@ module.exports = {
             "./logs/handlers/plugins.log",
             `[PLUGINS] Error al eliminar los archivos y carpetas de logs: ${error.message}`
           );
-          sendError(totoro, msg, `Error al eliminar los archivos y carpetas de logs: ${error.message}`);
+          sendError(
+            totoro,
+            msg,
+            `Error al eliminar los archivos y carpetas de logs: ${error.message}`
+          );
           return;
         }
-        
+
         if (stderr) {
           totoroLog.error(
             "./logs/handlers/plugins.log",
             `[PLUGINS] Error al eliminar los archivos y carpetas de logs: ${stderr}`
           );
-          sendError(totoro, msg, `Error al eliminar los archivos y carpetas de logs: ${stderr}`);
+          sendError(
+            totoro,
+            msg,
+            `Error al eliminar los archivos y carpetas de logs: ${stderr}`
+          );
           return;
         }
 
         if (stdout) {
-          totoroLog.info(
-            "./logs/handlers/plugins.log",
-            `[PLUGINS] ${stdout}`
-          );
-        } else { 
-          msg.reply( 
+          totoroLog.info("./logs/handlers/plugins.log", `[PLUGINS] ${stdout}`);
+        } else {
+          msg.reply(
             `╭──⬣「 Logs eliminados 」⬣\n` +
-            `│  ≡◦ 🐥 Todos los archivos y carpetas de logs eliminados.\n` +
-            `╰──⬣`
+              `│  ≡◦ 🐥 Todos los archivos y carpetas de logs eliminados.\n` +
+              `╰──⬣`
           );
-        }  
+        }
       });
-       
+
       await msg.react("🗑️");
     } catch (error) {
       totoroLog.error(
         "./logs/handlers/plugins.log",
         `[PLUGINS] Error al eliminar los archivos y carpetas de logs: ${error.message}`
       );
-      sendError(totoro, msg, `Error al eliminar los archivos y carpetas de logs: ${error.message}`);
+      sendError(
+        totoro,
+        msg,
+        `Error al eliminar los archivos y carpetas de logs: ${error.message}`
+      );
     }
-  }
+  },
 };

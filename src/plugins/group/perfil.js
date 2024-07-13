4445c4b3c988b808b805743aeb91034deda97401
lz20r <zhiyaonaiara@gmail.com>
2024-07-13 03:18:15 +0200

    feat: Agregar función para formatear números de teléfono
    Se ha agregado una nueva función en el archivo perfil.js que permite formatear números de teléfono. Esta función se utiliza para mostrar los números de teléfono de los usuarios en un formato legible. Además, se ha importado el módulo formatPhoneNumber para utilizar esta función en el código existente.

--- src/plugins/group/perfil.js
+++ src/plugins/group/perfil.js
@@ -1,5 +1,8 @@
 const totoUser = require("../../models/totoUser");
+const verifyUser = require("../../utils/verifyuser");
 const totoroLog = require("../../functions/totoroLog");
+const { sendWarning, sendError } = require("../../functions/messages");
+const formatPhoneNumber = require("../../utils/formatPhoneNumber"); // Función para formatear números
 
 module.exports = {
   name: "perfil",
@@ -9,39 +12,84 @@ module.exports = {
   usage: "perfil <@usuario>",
   aliases: ["profile", "p"],
 
-
-
-  async execute(totoro, msg, args, totoUser, sendWarning, countTotoUsers) {
-    const remoteJid = msg.messages[0].key.remoteJid;
-    const userCount = await countTotoUsers();
-    const totoU = await totoUser.findOne({ where: { phone: totoU } });
-    if (!totoU) {
-      await sendWarning(totoro, msg, "No se encontró el usuario solicitado.");
-      return;
-    }
-
-    if (!args[0]) {
-      await sendWarning(totoro, msg, "Debes mencionar a un usuario.");
-      return;
-    }
-
-    const profileMessage =
-      `╭─⬣「 *Perfil de ${totoU.name}* 」⬣\n` +
-      `│  ≡◦ *🍭 Nombre*: ${totoU.name}\n` +
-      `│  ≡◦ *🍭 Edad*: ${totoU.age}\n` +
-      `│  ≡◦ *🍭 País*: ${totoU.country}\n` +
-      `│  ≡◦ *🍭 Serial Number*: ${totoU.serialNumber}\n` +
-      `│  ≡◦ *🍭 Número de teléfono*: ${totoU.phone}\n` +
-      `│  ≡◦ *🍭 totoUser ID*: ${totoU.id}\n` +
-      `╰─⬣\n\n` +
-      `> *Contigo somos ${userCount} totoUsers*;`;
-
+  async execute(totoro, msg, args) {
     try {
-      await totoro.sendMessage(remoteJid, { text: profileMessage });
+      // Aseguramos que el objeto msg y sus propiedades están definidas
+      const participant = msg.messages?.[0]?.key?.participant;
+      const remoteJid = msg.messages?.[0]?.key?.remoteJid;
+      const pushname = msg.pushName;
+
+      // Verificamos la existencia de ambos participant y remoteJid
+      if (!participant && !remoteJid) {
+        return sendError(
+          totoro,
+          msg,
+          "No se pudo obtener el número del usuario o el chat."
+        );
+      }
+
+      // Verificamos si el usuario está registrado
+      let user;
+      try {
+        user = await verifyUser(participant || remoteJid);
+      } catch (error) {
+        if (
+          error.message ===
+          "No estás registrado. Por favor, regístrate antes de usar este comando."
+        ) {
+          return sendWarning(
+            totoro,
+            msg,
+            `No estás registrado. Por favor, regístrate antes de usar el comando ${this.name}.`
+          );
+        } else {
+          return sendError(totoro, msg, error.message);
+        }
+      }
+
+      let totoU;
+      if (args[0]) {
+        // Si se menciona a otro usuario, buscamos su perfil
+        const mentionedPhone = args[0];
+        totoU = await totoUser.findOne({ where: { phone: mentionedPhone } });
+
+        if (!totoU) {
+          return sendWarning(
+            totoro,
+            msg,
+            "No se encontró el usuario solicitado."
+          );
+        }
+      } else {
+        // Si no se menciona a otro usuario, mostramos el perfil del usuario que ejecutó el comando
+        totoU = await totoUser.findOne({ where: { phone: user.phone } });
+      }
+
+      const formattedPhone = formatPhoneNumber(totoU.phone);
+      const profileMessage =
+        `╭─⬣「 *Perfil de ${totoU.name}* 」⬣\n` +
+        `│  ≡◦ *🍭 Nombre*: ${totoU.name} (${pushname})\n` +
+        `│  ≡◦ *🍭 Número de teléfono*: ${formattedPhone}\n` +
+        `│  ≡◦ *🍭 LINK*: wa.me/${totoU.phone.replace(/\D/g, "")}\n` +
+        `│  ≡◦ *🍭 Edad*: ${totoU.age} años\n` +
+        `│  ≡◦ *🍭 Registrado*: ${totoU.registered ? "Sí" : "No"}\n` + // Indica si el usuario está registrado
+        `│  ≡◦ *🍭 Premium*: ${totoU.isPremium ? "Sí" : "No"}\n` +
+        `│  ≡◦ *🍭 Número de serie*: ${totoU.serialNumber}\n` +
+        `╰─⬣\n\n` +
+        `> 🍭 Gracias por usar Totoro`;
+
+      await totoro.sendMessage(remoteJid || participant, {
+        text: profileMessage,
+      });
     } catch (error) {
       totoroLog.error(
         "./logs/plugins/group/perfil.log",
         `Error enviando mensaje de perfil: ${error}`
+      );
+      await sendError(
+        totoro,
+        msg,
+        "Hubo un error al intentar mostrar el perfil del usuario."
       );
     }
   },

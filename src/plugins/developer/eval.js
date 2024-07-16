bc2af88a824c704f2f5e0d35bd33180a1d6290d5
lz20r <zhiyaonaiara@gmail.com>
2024-07-16 13:42:11 +0200

    feat: Agregar archivo eval.js para ejecutar código
    Se ha agregado el archivo eval.js que contiene un plugin para ejecutar código. Este plugin permite ejecutar código utilizando el comando "eval". Mejora la capacidad del sistema para realizar evaluaciones de código y proporciona una opción adicional para los desarrolladores.

---
+++ src/plugins/developer/eval.js
@@ -0,0 +1,52 @@
+const { sendError, help } = require("../../functions/messages");
+const totoroLog = require("../../functions/totoroLog");
+
+module.exports = {
+  name: "eval",
+  aliases: ["e"],
+  category: "developer",
+  subcategory: "owner",
+  description: "Ejecuta codigo",
+  usage: "< codigo >",
+  example: 'eval console.log("Hello World")',
+  dev: true,
+
+  async execute(totoro, msg, args) {
+    const info = msg.messages[0];
+    const from = info.key.remoteJid;
+    const reply = (text) => {
+      totoro.sendMessage(from, { text: text }, { quoted: info });
+    };
+
+    if (!args.join(" ")) {
+      return help(totoro, msg, "Eval", "Ejecuta codigo", "+eval <codigo>");
+    }
+
+    try {
+      const code = args.join(" ");
+      /* eslint no-eval: 0 */
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string") {
+        evaled = require("util").inspect(evaled);
+      }
+
+      reply(clean(evaled));
+    } catch (e) {
+      totoroLog.error(
+        "./logs/plugins/developer/eval.log",
+        `Error ejecutando eval: ${e}`
+      );
+      sendError(totoro, msg, e);
+    }
+  },
+};
+
+function clean(text) {
+  if (typeof text === "string") {
+    return text
+      .replace(/`/g, `\`${String.fromCharCode(8203)}`)
+      .replace(/@/g, `@${String.fromCharCode(8203)}`);
+  }
+  return text;
+}

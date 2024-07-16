aa8f84da83018e70bb1e51a9e4e370f932daf12b
lz20r <zhiyaonaiara@gmail.com>
2024-07-16 13:39:40 +0200

    feat: Agregar función de texto a voz
    Se ha agregado la función de texto a voz en el archivo tts.js. Esta función permite convertir texto en voz y enviar un audio. Mejora la funcionalidad del sistema al proporcionar una forma de comunicación auditiva.

---
+++ src/plugins/audio/tts.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+const {
+  sendWarning,
+  sendError,
+  sendMessage,
+  sendSuccess,
+} = require("../../functions/messages");
+
+module.exports = {
+  name: "textToSpeech",
+  usage: "tts <texto>",
+  aliases: ["tts"],
+  category: "Audio",
+  subcategory: "Voz",
+  description: "Convierte texto a voz y envía un audio",
+
+  async execute(totoro, msg, args) {
+    const info = msg.messages[0];
+    const from = info.key.remoteJid;
+
+    if (!args.length) {
+      return help(
+        totoro,
+        msg,
+        "tts",
+        "Debes escribir un texto para convertir a voz",
+        "+tts <texto>"
+      );
+    }
+
+    const text = args.join(" ");
+
+    const voices = ["Elena", "Tomas"];
+    const selectedVoice = voices[Math.floor(Math.random() * voices.length)];
+
+    const apiUrl = `https://api.kastg.xyz/api/ai/tts?lang=Spanish&voice=${selectedVoice}&input=${encodeURIComponent(text)}`;
+
+    try {
+      const response = await axios.get(apiUrl);
+      if (response.data.status === "true" && response.data.result.length > 0) {
+        const audioUrl = response.data.result[0].url;
+        const audioResponse = await axios.get(audioUrl, {
+          responseType: "arraybuffer",
+        });
+        const audioBuffer = Buffer.from(audioResponse.data, "binary");
+        await msg.react("🔊");
+
+        await totoro.sendMessage(from, {
+          audio: audioBuffer,
+          mimetype: "audio/mp4",
+          ptt: true,
+        });
+
+        await msg.react("🔊");
+      } else {
+        sendWarning(totoro, msg, `${response.data.message}`);
+      }
+    } catch (error) {
+      console.error(error);
+      sendError(totoro, msg, `Error convirtiendo texto a voz: ${error}`);
+    }
+  },
+};

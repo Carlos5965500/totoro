69c6b270ac21fd5b57fde4fbab4208912cde4eda
lz20r <zhiyaonaiara@gmail.com>
2024-07-16 13:39:30 +0200

    feat: Actualizar carga de plugins y registro en el log
    Se ha actualizado la carga de plugins en el archivo plugins.js para utilizar la función readdir de fs/promises en lugar de fs.readdir. Además, se ha agregado el registro en el log de la cantidad de plugins cargados. Este cambio mejora la eficiencia y la visibilidad de la carga de plugins en el sistema.

--- src/handlers/plugins.js
+++ src/handlers/plugins.js
@@ -1,20 +1,42 @@
 const { readdir } = require("fs/promises");
 const totoroLog = require("../functions/totoroLog");
+
 module.exports = async (totoro) => {
-  const directory = await readdir("./src/plugins");
+  try {
+    const directory = await readdir("./src/plugins");
 
-  for (const folder of directory) {
-    const files = await readdir(`./src/plugins/${folder}`);
+    for (const folder of directory) {
+      const files = await readdir(`./src/plugins/${folder}`);
 
-    for (const file of files) {
-      delete require.cache[require.resolve(`../plugins/${folder}/${file}`)];
+      for (const file of files) {
+        const pluginPath = `../plugins/${folder}/${file}`;
+        delete require.cache[require.resolve(pluginPath)];
 
-      const plugin = require(`../plugins/${folder}/${file}`);
-      totoro.plugins.set(plugin.name, plugin);
+        // Cargar el módulo del plugin
+        const plugin = require(pluginPath);
+
+        // Añadir el nombre principal del plugin al mapa de plugins
+        totoro.plugins.set(plugin.name.toLowerCase(), plugin);
+
+        // Añadir aliases (si existen) al mapa de plugins
+        if (plugin.aliases && Array.isArray(plugin.aliases)) {
+          for (const alias of plugin.aliases) {
+            totoro.plugins.set(alias.toLowerCase(), plugin);
+          }
+        }
+      }
     }
+
+    // Registrar en el log la cantidad de plugins cargados
+    totoroLog.info(
+      "./logs/handlers/plugins.log",
+      `[PLUGINS] ${totoro.plugins.size} cargados.`
+    );
+  } catch (error) {
+    console.error("Error al cargar plugins:", error);
+    totoroLog.error(
+      "./logs/handlers/plugins.log",
+      `[ERROR] Error al cargar plugins: ${error.message}`
+    );
   }
-  totoroLog.info(
-    "./logs/handlers/plugins.log",
-    `[PLUGINS] ${totoro.plugins.size} cargados.`
-  );
 };

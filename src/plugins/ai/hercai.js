1ea459311434b711ab651f4417ed148080297d8d
lz20r <zhiyaonaiara@gmail.com>
2024-07-16 13:40:52 +0200

    feat: Agregar plugin de AI Hercai
    Se ha agregado el archivo hercai.js que contiene un plugin de AI llamado Hercai. Este plugin permite utilizar la funcionalidad de AI para responder a mensajes utilizando el modelo "v3". Mejora la capacidad de respuesta del sistema al proporcionar una opción adicional de AI para interactuar con los usuarios.

---
+++ src/plugins/ai/hercai.js
@@ -0,0 +1,37 @@
+const { Hercai } = require("hercai");
+const { help, sendError } = require("../../functions/messages");
+
+module.exports = {
+  name: "hercai",
+  category: "ai",
+  description: "Hercai AI",
+  usage: "hercai <message>",
+  aliases: ["hercai", "hercai"],
+  async execute(totoro, msg, args) {
+    try {
+      const message = args.join(" ");
+      if (!message) {
+        await help(
+          totoro,
+          msg,
+          "Hercai AI",
+          "Ingresa un mensaje",
+          "+hercai <message>"
+        );
+        await msg.react("❓");
+        return;
+      }
+
+      const hercai = new Hercai();
+      const response = await hercai.question({
+        content: message,
+        model: "v3",
+      });
+
+      // Enviar la respuesta usando msg.reply
+      await msg.reply(response.reply);
+    } catch (error) {
+      await sendError(totoro, msg, error.message);
+    }
+  },
+};

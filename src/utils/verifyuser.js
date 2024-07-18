6e3ad840cab1c25729ec357b94e12c133d11a884
lz20r <zhiyaonaiara@gmail.com>
2024-07-18 13:46:32 +0200

    feat: Actualizar función de verificación de usuario
    Se ha actualizado la función de verificación de usuario en el archivo verifyuser.js para mejorar la claridad y consistencia del código. Se ha eliminado la variable no utilizada "dev" y se han agregado mensajes de registro para informar al usuario sobre el resultado de la verificación. Estos cambios mejoran la funcionalidad del sistema y brindan una mejor experiencia de usuario al utilizar la aplicación.

--- src/utils/verifyuser.js
+++ src/utils/verifyuser.js
@@ -2,7 +2,6 @@ const totoUser = require("../models/totoUser");
 const totoroLog = require("../functions/totoroLog");
 const { infoRegister, sendError } = require("../functions/messages");
 const config = require("../../settings.json");
-const dev = config.dev;
 
 async function verifyUser(participant, totoro, msg) {
   try {
@@ -14,27 +13,36 @@ async function verifyUser(participant, totoro, msg) {
       await infoRegister(
         totoro,
         msg,
-        `Por favor, regístrate con el siguiente comando: ${totoroPrefix}reg nombre.edad`
+        `Por favor, regístrate con el siguiente comando: ${totoroPrefix}register nombre.edad`
       );
 
-      totoroLog.info(
-        "./logs/utils/verifyuser.log",
-        `Usuario no registrado: ${phone}`
-      );
-      return null;
+      totoroLog.verbose
+        ? totoroLog.verbose(
+            "./logs/utils/verifyuser.log",
+            `El ${user} con el número de teléfono ${phone} no está registrado.`
+          )
+        : null;
+    } else {
+      totoroLog.verbose
+        ? totoroLog.verbose(
+            "./logs/utils/verifyuser.log",
+            `El ${user} con el número de teléfono ${phone} está registrado.`
+          )
+        : null;
     }
     return user;
   } catch (error) {
-    console.error("Error verificando usuario:", error);
+    console.error("Error verificando ${user}:", error);
     await sendError(
       totoro,
       msg,
-      `> Hubo un error al verificar tu registro. Por favor, inténtalo de nuevo más tarde.`
+      `Hubo un error al verificar tu registro. Por favor, inténtalo de nuevo más tarde. Gracias.\n ${error}`
     );
 
+    // Asegúrate de que el primer argumento es un string y no una función
     totoroLog.error(
       "./logs/utils/verifyuser.log",
-      `Error verificando usuario: ${error}`
+      `Error verificando ${user}: ${error.message || error}`
     );
     return null;
   }

b2f24a957b39b67f970cf95ea1e579a8be4afc55
lz20r <zhiyaonaiara@gmail.com>
2024-07-19 00:51:36 +0200

    feat: Agregar campo de estado a modelo de lista negra de usuarios
    Se ha agregado un campo de estado al modelo de lista negra de usuarios en el archivo totoBlackList.js. Este campo permite identificar el estado de un usuario en la lista negra y se utiliza para bloquear el acceso a ciertas funcionalidades. Estos cambios mejoran la funcionalidad del sistema y brindan una mejor experiencia de usuario al utilizar la aplicación.

--- src/models/totoBlackList.js
+++ src/models/totoBlackList.js
@@ -1,21 +1,32 @@
 const { DataTypes } = require("sequelize");
 const TotoDB = require("../libs/db/totoDB");
-const totoUser = require("./totoUser");
-
+const totoroLog = require("../functions/totoroLog");
 const tDB = new TotoDB();
+const totoUser = require("./totoUser");
 
 const totoBlacklist = tDB.sequelize.define(
   "totoBlacklist",
   {
     userId: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
       references: {
         model: totoUser,
         key: "id",
       },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    },
+    userPhone: {
+      type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-      primaryKey: true,
+      references: {
+        model: totoUser,
+        key: "phone",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
     },
   },
   {
@@ -24,7 +35,9 @@ const totoBlacklist = tDB.sequelize.define(
   }
 );
 
-totoUser.hasOne(totoBlacklist, { foreignKey: "userId" });
-totoBlacklist.belongsTo(totoUser, { foreignKey: "userId" });
+totoroLog.info(
+  "./logs/models/totoBlacklist.log",
+  `[MODELS] Modelo ${totoBlacklist.name} creado.`
+);
 
 module.exports = totoBlacklist;

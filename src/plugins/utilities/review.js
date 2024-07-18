964904703168f4522d872b8dd7b8a407675aa816
lz20r <zhiyaonaiara@gmail.com>
2024-07-19 00:53:33 +0200

    feat: Agregar función de revisión de sugerencias
    Se ha agregado la función de revisión de sugerencias en el archivo review.js en el directorio src/plugins/utilities. Esta función permite revisar y enviar sugerencias a través de mensajes interactivos en WhatsApp. Mejora la funcionalidad del sistema al proporcionar a los usuarios una forma conveniente de revisar y responder a las sugerencias recibidas.

---
+++ src/plugins/utilities/review.js
@@ -0,0 +1,57 @@
+const { prepareWAMessageMedia } = require("@whiskeysockets/baileys");
+
+module.exports = {
+  name: "review",
+  category: "utilities",
+  subcategory: "c",
+  usage: "c",
+  description: "c",
+
+  async execute(totoro, msg, args) {
+    const review = args.join(" ");
+
+    if (!review) return msg.reply("Totoro Reviews");
+
+    const user = msg.messages[0]?.pushName || ".";
+
+    const content = `*Sugerencia de ${user}*\n${review}`;
+
+    const { imageMessage } = await prepareWAMessageMedia(
+      {
+        image: { url: "https://i.ibb.co/j9N5kj3/image.jpg" },
+      },
+      { upload: totoro.waUploadToServer }
+    );
+
+    const message = {
+      interactiveMessage: {
+        header: {
+          hasMediaAttachment: true,
+          imageMessage: imageMessage,
+        },
+        body: { text: content },
+        footer: { text: "Totoro Reviews" },
+        nativeFlowMessage: {
+          buttons: [
+            {
+              name: "quick_reply",
+              buttonParamsJson: JSON.stringify({
+                display_text: "Review",
+                id: `sendReview+${review}`,
+              }),
+            },
+          ],
+          messageParamsJson: "",
+        },
+      },
+    };
+
+    totoro.relayMessage(
+      msg.messages[0].key.remoteJid,
+      { viewOnceMessage: { message } },
+      {
+        quoted: msg.messages[0],
+      }
+    );
+  },
+};

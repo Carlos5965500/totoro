4a7d0607126c213586be4e2ea3775ad73c826c32
lz20r <zhiyaonaiara@gmail.com>
2024-07-19 19:21:56 +0200

    feat: Agregar soporte para descargar imágenes de Instagram
    Agrega la funcionalidad de descargar imágenes de Instagram a través del comando "igmedia" en el archivo igmedia.js en el directorio src/plugins/multimedia. Esto permite a los usuarios descargar imágenes de Instagram de manera conveniente, mejorando la experiencia de usuario al utilizar la aplicación.

---
+++ src/plugins/multimedia/igmedia.js
@@ -0,0 +1,105 @@
+const Scraper = require("@SumiFX/Scraper");
+const { sendError, help, sendWarning } = require("../../functions/messages");
+const totoroLog = require("../../functions/totoroLog");
+
+module.exports = {
+  name: "igmedia",
+  category: "multimedia",
+  subcategory: "instagram",
+  usage: "igimage <enlace>",
+  description: "Descarga imágenes de Instagram",
+
+  async execute(totoro, msg, args) {
+    msg.react("⏳");
+    if (!args[0]) {
+      return help(
+        totoro,
+        msg,
+        "igmedia",
+        "Descarga imágenes de Instagram",
+        "igmedia <enlace>"
+      );
+    }
+
+    try {
+      let { dl_url } = await Scraper.igdl(args[0]);
+      if (!dl_url) {
+        totoroLog.info(
+          "./logs/plugins/multimedia/igimage.log",
+          "Error al obtener el enlace de descarga."
+        );
+        return sendWarning(
+          totoro,
+          msg,
+          "No se pudo obtener el enlace de descarga."
+        );
+      }
+
+      // solo quiero se pueda descargar la imagen
+      if (!dl_url.match(/.jpg|.jpeg|.png/gi)) {
+        totoroLog.info(
+          "./logs/plugins/multimedia/igimage.log",
+          "No se pudo obtener el enlace de descarga."
+        );
+        return sendWarning(totoro, msg, `Solo se pueden descargar imágenes.`);
+      }
+
+      if (!msg.messages || msg.messages.length === 0) {
+        sendWarning(
+          totoro,
+          msg,
+          "No se pudo obtener el mensaje citado. Por favor, vuelve a intentarlo."
+        );
+
+        totoroLog.info(
+          "./logs/plugins/multimedia/igimage.log",
+          `No se pudo obtener el mensaje citado.
+          msg.messages: ${JSON.stringify(msg.messages)}`
+        );
+      }
+
+      const message = msg.messages[0];
+      const remoteJid = message.key.remoteJid;
+
+      if (!message.key || !remoteJid) {
+        sendError(
+          totoro,
+          msg,
+          `No se pudo obtener el número de usuario o el chat.
+            message.key: ${JSON.stringify(message.key)}`
+        );
+
+        totoroLog.info(
+          "./logs/plugins/multimedia/igimage.js",
+          `No se pudo obtener el número de usuario o el chat.
+          message.key: ${JSON.stringify(message.key)}`
+        );
+      }
+
+      let txt = `╭─⬣「 *Instagram Image Download* 」⬣\n`;
+      txt += `│  ≡◦ *🍭 Enlace ∙* ${args[0]}\n`;
+      txt += `╰─⬣`;
+
+      await totoro.sendMessage(
+        remoteJid,
+        {
+          image: { url: dl_url },
+          caption: txt,
+        },
+        { quoted: message }
+      );
+
+      await msg.react("📷");
+    } catch (error) {
+      totoroLog.error(
+        "./logs/plugins/multimedia/igimage.js",
+        `Error al descargar la imagen de Instagram: ${error}`
+      );
+      sendError(
+        totoro,
+        msg,
+        "Error al descargar la imagen de Instagram. Por favor, vuelve a intentarlo."
+      );
+    }
+  },
+};

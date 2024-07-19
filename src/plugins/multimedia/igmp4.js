bed290cd2f7070e85d7471188f7dc23eff119b57
lz20r <zhiyaonaiara@gmail.com>
2024-07-19 19:21:27 +0200

    feat: Agregar soporte para descarga de videos de Instagram
    Agrega la funcionalidad de descarga de videos de Instagram a través del comando "igmp4" en el archivo igmp4.js en el directorio src/plugins/multimedia. Esto permite a los usuarios descargar videos de Instagram de manera conveniente, mejorando la experiencia de usuario al utilizar la aplicación.

---
+++ src/plugins/multimedia/igmp4.js
@@ -0,0 +1,114 @@
+const Scraper = require("@SumiFX/Scraper");
+const { sendError, help, sendWarning } = require("../../functions/messages");
+const totoroLog = require("../../functions/totoroLog");
+module.exports = {
+  name: "igmp4",
+  category: "multimedia",
+  subcategory: "instagram",
+  usage: "igmp4 <enlace>",
+  description: "Descarga video de ig",
+
+  async execute(totoro, msg, args) {
+    msg.react("⏳");
+    if (!args[0]) {
+      return help(
+        totoro,
+        msg,
+        "igmp4",
+        "Descarga video de ig",
+        "igmp4 <enlace>"
+      );
+    }
+
+    try {
+      let { dl_url } = await Scraper.igdl(args[0]);
+      if (!dl_url) {
+        totoroLog.info(
+          "./logs/plugins/multimedia/igmp4.log",
+          "Error al obtener el enlace de descarga."
+        );
+        return sendWarning(
+          totoro,
+          msg,
+          "No se pudo obtener el enlace de descarga."
+        );
+      }
+
+      // solo quiero se pueda descargar la imagen
+      if (dl_url.match(/.jpg|.jpeg|.png/gi)) {
+        // Cambiar a .mp4
+        totoroLog.info(
+          "./logs/plugins/multimedia/igmp4.log",
+          "No se pudo obtener el enlace de descarga."
+        );
+        return sendWarning(totoro, msg, `No se pueden descargar imágenes.`);
+      } else if (dl_url.match(/.mp4/gi)) {
+        totoroLog.info(
+          "./logs/plugins/multimedia/igmp4.log",
+          "Enlace de descarga obtenido."
+        );
+      }
+
+      // Asegurarse de que msg.messages existe y tiene al menos un mensaje
+      if (!msg.messages || msg.messages.length === 0) {
+        sendWarning(
+          totoro,
+          msg,
+          "No se pudo obtener el mensaje citado. Por favor, vuelve a intentarlo."
+        );
+
+        totoroLog.info(
+          "./logs/plugins/multimedia/igmp4.log",
+          `No se pudo obtener el mensaje citado.
+          msg.messages: ${JSON.stringify(msg.messages)}`
+        );
+      }
+
+      const message = msg.messages[0];
+      const remoteJid = message.key.remoteJid;
+
+      // Asegurarse de que el mensaje y remoteJid son válidos
+      if (!message.key || !remoteJid) {
+        sendError(
+          totoro,
+          msg,
+          `No se pudo obtener el número de usuario o el chat.
+            message.key: ${JSON.stringify(message.key)}`
+        );
+
+        totoroLog.info(
+          "./logs/plugins/multimedia/igmp4.js",
+          `No se pudo obtener el número de usuario o el chat.
+          message.key: ${JSON.stringify(message.key)}`
+        );
+      }
+
+      let txt = `╭─⬣「 *Instagram Video Download* 」⬣\n`;
+      txt += `│  ≡◦ *🍭 Enlace ∙* ${args[0]}\n`;
+      txt += `╰─⬣`;
+
+      // Enviar el mensaje de video
+      await totoro.sendMessage(
+        remoteJid,
+        {
+          video: { url: dl_url },
+          caption: txt,
+        },
+        { quoted: message }
+      );
+
+      // Reaccionar al mensaje original
+      await msg.react("📽️");
+    } catch (error) {
+      totoroLog.error(
+        "./logs/plugins/multimedia/igmp4.js",
+        `Error al descargar el video de Instagram: ${error}`
+      );
+      sendError(
+        totoro,
+        msg,
+        "Error al descargar el video de Instagram. Por favor, vuelve a intentarlo."
+      );
+    }
+  },
+};

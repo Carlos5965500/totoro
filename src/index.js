865a2177d4d87876a2edb0aea8a50054d51eed65
lz20r <zhiyaonaiara@gmail.com>
2024-07-22 16:30:39 +0200

    feat: Actualizar directorio de autenticación y emparejamiento a "totoro"
    Actualiza el directorio de autenticación y emparejamiento a "totoro" en lugar de "momo" en los archivos index.js y connectToTotoro() para reflejar el cambio de nombre del directorio. Esto asegura que los archivos se guarden en el directorio correcto y mejora la consistencia en la aplicación.

--- src/index.js
+++ src/index.js
@@ -9,20 +9,20 @@ const discord = require("@discordjs/collection");
 const pino = require("pino");
 const fs = require("fs");
 const readline = require("readline");
-const path = require("path"); 
+const path = require("path");
 
 // Carga las variables de entorno desde .env
 require("dotenv").config();
 
 process.loadEnvFile = function () {
   require("dotenv").config();
-}; 
+};
 
 // Define el directorio donde se guardará pairing_data.json
-const PAIRING_DATA_DIR = path.join(__dirname, "..", "auth", "momo-pairing"); // Reemplaza 'pairing_data_directory' con tu directorio deseado
+const PAIRING_DATA_DIR = path.join(__dirname, "..", "auth", "totoro-pairing"); // Reemplaza 'pairing_data_directory' con tu directorio deseado
 
-// Define el directorio donde se guardará momo-auth
-const AUTH_DATA_DIR = path.join(__dirname, "..", "auth", "momo-auth"); // Reemplaza 'auth_directory' con tu directorio deseado
+// Define el directorio donde se guardará totoro-auth
+const AUTH_DATA_DIR = path.join(__dirname, "..", "auth", "totoro-auth"); // Reemplaza 'auth_directory' con tu directorio deseado
 
 // Crear los directorios si no existen de manera recursiva
 if (!fs.existsSync(PAIRING_DATA_DIR)) {
@@ -50,7 +50,7 @@ async function connectToTotoro() {
   // Manejo de errores para la autenticación
   let state, saveCreds;
   try {
-    // Utiliza el directorio especificado para guardar momo-auth
+    // Utiliza el directorio especificado para guardar totoro-auth
     ({ state, saveCreds } = await useMultiFileAuthState(AUTH_DATA_DIR));
   } catch (err) {
     console.error("Error al obtener el estado de autenticación:", err);
@@ -59,7 +59,7 @@ async function connectToTotoro() {
 
   const logger = pino({ level: "silent" });
 
-  const momo = makeWASocket({
+  const totoro = makeWASocket({
     logger: logger,
     printQRInTerminal: false,
     browser: ["Mac OS", "chrome", "121.0.6167.159"],
@@ -70,14 +70,14 @@ async function connectToTotoro() {
     msgRetryCounterCache,
   });
 
-  momo.ev.on("creds.update", saveCreds);
+  totoro.ev.on("creds.update", saveCreds);
 
-  if (!momo.authState.creds.registered) {
+  if (!totoro.authState.creds.registered) {
     await delay(5000);
     const phoneNumber = await question(
       "Ingresa tu número de teléfono de WhatsApp: "
     );
-    const code = await momo.requestPairingCode(phoneNumber);
+    const code = await totoro.requestPairingCode(phoneNumber);
     console.log(`Emparejando con este código: ${code}`);
 
     // Guardar pairing_data.json en el directorio específico
@@ -85,8 +85,8 @@ async function connectToTotoro() {
     fs.writeFileSync(pairingDataPath, JSON.stringify({ code }, null, 2));
   }
 
-  momo.plugins = new discord.Collection();
-  momo.components = new discord.Collection();
+  totoro.plugins = new discord.Collection();
+  totoro.components = new discord.Collection();
 
   let folder;
   try {
@@ -101,7 +101,7 @@ async function connectToTotoro() {
     try {
       const handler = require(handlerPath);
       if (typeof handler === "function") {
-        handler(momo);
+        handler(totoro);
       }
     } catch (err) {
       console.error(`Error al cargar el handler ${file}:`, err);
@@ -109,7 +109,7 @@ async function connectToTotoro() {
   }
 
   try {
-    momo.config = require(path.join(__dirname, "..", "settings.json"));
+    totoro.config = require(path.join(__dirname, "..", "settings.json"));
   } catch (err) {
     console.error("Error al cargar settings.json:", err);
   }
@@ -120,8 +120,12 @@ const db = new totoDB();
 const syncTotoDB = require("../src/scripts/sync");
 const sync = new syncTotoDB();
 
-db.isConnected().catch((err) => console.log("Error connecting to the database: ", err));
-sync.sync().catch((err) => console.log("Error al sincronizar la base de datos: ", err));
+db.isConnected().catch((err) =>
+  console.log("Error connecting to the database: ", err)
+);
+sync
+  .sync()
+  .catch((err) => console.log("Error al sincronizar la base de datos: ", err));
 connectToTotoro().catch((err) => console.log("Error connecting to WA: ", err));
 
 module.exports = { connectToTotoro };

a7583106b9cc1cd6872e5b4c06541ee7760463f7
lz20r <zhiyaonaiara@gmail.com>
2024-07-23 22:30:35 +0200

    feat: Corregir importación de eventos en events.js
    Corrige la importación de eventos en el archivo events.js para asegurar que todos los eventos se carguen correctamente. Esto soluciona un error que causaba que algunos eventos no se activaran correctamente en la aplicación.

--- src/handlers/events.js
+++ src/handlers/events.js
@@ -6,7 +6,6 @@ module.exports = async (totoro) => {
 
   for (const file of folder) {
     const event = require(`../events/${file}`);
-
     totoro.ev.on(event.name, (...args) => {
       event.load(...args, totoro);
     });

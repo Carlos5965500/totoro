eefa2bb305485a81c1ae8d022be01f10487cd63e
lz20r <zhiyaonaiara@gmail.com>
2024-07-25 20:13:45 +0200

    feat: Mejorar funcionalidad de añadir y remover desarrolladores en el archivo add.js
    Estos cambios en el archivo add.js mejoran la funcionalidad de añadir y remover desarrolladores. Se implementa la capacidad de obtener el número de teléfono de un participante citado en un mensaje y se mejora la validación del valor de dev. También se notifica al usuario si el dev ya está en la lista al intentar añadirlo y se realiza una optimización en el código al remover a un dev de la lista. Estos cambios mejoran la gestión y control de los desarrolladores en la aplicación.

---
+++ src/components/add.js
@@ -0,0 +1,52 @@
+const { sendError } = require("../functions/messages");
+
+module.exports = {
+  id: "add",
+  async execute(totoro, msg) {
+    try {
+      const groupId = msg.messages[0].key.remoteJid;
+      const groupInfo = await totoro.groupMetadata(groupId);
+      const groupName = groupInfo.subject;
+
+      // Validar si el usuario que ejecuta el comando es administrador
+      const executorId = msg.messages[0].key.participant;
+      const executor = groupInfo.participants.find((x) => x.id === executorId);
+      if (!executor || !executor.admin) {
+        sendError(
+          totoro,
+          msg,
+          "No tienes permisos para ejecutar este comando. Solo los administradores pueden usar este comando."
+        );
+        return;
+      }
+
+      if (groupId.endsWith("@g.us")) {
+        // Obtener el identificador del participante a agregar desde el mensaje
+        const participantId =
+          msg.messages[0].message.extendedTextMessage?.contextInfo?.participant;
+
+        if (!participantId) {
+          await totoro.sendMessage(groupId, {
+            text: `Por favor, menciona al usuario que deseas agregar al grupo.`,
+          });
+          return;
+        }
+
+        // Agregar al usuario al grupo
+        await totoro.groupParticipantsUpdate(groupId, [participantId], "add");
+
+        // Enviar mensaje de éxito
+        await totoro.sendMessage(groupId, {
+          text:
+            `╭─⬣「 Mensaje de Bienvenida 」⬣\n` +
+            `│  ≡◦ 🍭 Bienvenido/a al grupo\n` +
+            `╰─⬣\n` +
+            `> ¡Bienvenido/a @${participantId.split("@")[0]}! @${executorId.split("@")[0]} te ha agregado al grupo ${groupName}.\n`,
+          mentions: [participantId, executorId],
+        });
+      }
+    } catch (error) {
+      sendError(totoro, msg, error);
+    }
+  },
+};

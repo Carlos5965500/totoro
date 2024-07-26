38091ea7f21c184487aed33e670261ad8c6a4909
lz20r <zhiyaonaiara@gmail.com>
2024-07-26 12:06:39 +0200

    feat: Agregar funcionalidad de registro de usuarios premium
    Se agrega el archivo "premium.js" en la carpeta de plugins/forms para implementar la funcionalidad de registro de usuarios premium. Este archivo contiene la lógica para registrar un usuario premium en la base de datos, utilizando un número de serie como argumento. Se realizan validaciones y se actualiza el estado del usuario en la base de datos. Además, se envía un mensaje de confirmación al usuario. Estos cambios mejoran la funcionalidad del bot al permitir a los usuarios registrarse como usuarios premium.

---
+++ src/plugins/forms/premium.js
@@ -0,0 +1,98 @@
+const {
+  sendError,
+  help,
+  sendPrem,
+  sendWarning,
+} = require("../../functions/messages");
+const { totoUser, totoPremium } = require("../../models");
+
+module.exports = {
+  name: "registerPremium",
+  category: "forms",
+  subcategory: "register",
+  description: "Registra un usuario premium",
+  usage: "premium <serial>",
+  aliases: ["premium"],
+
+  async execute(totoro, msg, args) {
+    try {
+      const remoteJid = msg.messages[0].key.remoteJid;
+      const participant = msg.messages[0].key.participant;
+      const [serial] = args;
+
+      // que no esté permitido en grupos
+      if (remoteJid.endsWith("@g.us")) {
+        await sendWarning(
+          totoro,
+          msg,
+          "Este comando no está permitido en grupos."
+        );
+        return;
+      }
+
+      if (!serial) {
+        await help(
+          totoro,
+          msg,
+          "Registro de Usuario Premium",
+          "Ingresa tu número serial",
+          "+regPremium <serial>"
+        );
+        await msg.react("❓");
+        return;
+      }
+
+      const telf = participant || remoteJid;
+      const phone = telf.split("@")[0];
+
+      if (!/^\d+$/.test(phone)) {
+        await sendError(
+          totoro,
+          msg,
+          `Número de teléfono ${phone} no es válido.`
+        );
+        return;
+      }
+
+      const user = await totoUser.findOne({ where: { phone } });
+
+      if (!user) {
+        await sendError(
+          totoro,
+          msg,
+          `Usuario con teléfono ${phone} no encontrado.`
+        );
+        return;
+      } else {
+        await totoUser.update({ premium: true }, { where: { phone } });
+
+        const premiumRecord = await totoPremium.findOne({
+          where: { totoUserId: user.id },
+        });
+
+        if (!premiumRecord) {
+          await totoPremium.create({
+            totoUserId: user.id,
+            serialNumber: serial,
+          });
+        }
+
+        await sendPrem(
+          totoro,
+          remoteJid,
+          user.name,
+          user.age,
+          user.phone,
+          user.country,
+          serial,
+          await totoUser.count({ where: { premium: true } })
+        );
+      }
+
+      await msg.react("✅");
+    } catch (error) {
+      console.error("Error al registrar el usuario premium:", error);
+      await sendError(totoro, msg, "Error al registrar el usuario premium.");
+    }
+  },
+};
---
+++ src/plugins/forms/serial.js
@@ -0,0 +1,86 @@
+const axios = require("axios");
+const {
+  sendError,
+  sendSerial,
+  sendWarning,
+} = require("../../functions/messages");
+const { totoUser } = require("../../models");
+
+module.exports = {
+  name: "getSerialNumber",
+  category: "forms",
+  subcategory: "register",
+  description: "Generar un número de serie para un usuario",
+  usage: "getSerialNumber",
+  aliases: ["getserial", "serialnumber", "serial"],
+
+  async execute(totoro, msg, args) {
+    try {
+      const remoteJid = msg.messages[0].key.remoteJid;
+      const participant = msg.messages[0].key.participant;
+
+      if (remoteJid.endsWith("@g.us")) {
+        await sendWarning(
+          totoro,
+          msg,
+          "Este comando no está permitido en grupos."
+        );
+        return;
+      }
+      const telf = participant || remoteJid;
+      const phone = telf.split("@")[0];
+
+      // Validación del formato del número de teléfono
+      if (!/^\d+$/.test(phone)) {
+        await sendError(
+          totoro,
+          msg,
+          `Número de teléfono ${phone} no es válido.`
+        );
+        return;
+      }
+
+      const user = await totoUser.findOne({ where: { phone } });
+
+      if (!user) {
+        await sendError(
+          totoro,
+          msg,
+          `Usuario con el número ${phone} no encontrado.`
+        );
+        return;
+      }
+
+      // Realiza una solicitud a la API para generar el número de serie
+      try {
+        const response = await axios.get(
+          `https://cinammon.es/totoro/totoLicense.php?phone=${phone}`
+        );
+
+        // Verificar la estructura de la respuesta
+        if (response.data && response.data.totoLicense) {
+          const serialNumber = response.data.totoLicense;
+
+          await sendSerial(msg, user.name, serialNumber);
+          msg.reply(`${serialNumber}`);
+        } else {
+          console.error("Respuesta inesperada de la API:", response.data);
+          await sendError(
+            totoro,
+            msg,
+            "Error al generar el número de serie. Respuesta inesperada de la API."
+          );
+        }
+      } catch (apiError) {
+        console.error("Error al generar el número de serie:", apiError);
+        await sendError(totoro, msg, "Error al generar el número de serie.");
+        return;
+      }
+
+      await msg.react("✅");
+    } catch (error) {
+      console.error("Error al procesar el comando:", error);
+      await sendError(totoro, msg, "Error al procesar el comando.");
+    }
+  },
+};

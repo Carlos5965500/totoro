6762b595dc48e671a22ccfe8f24a13c3cdc48d0a
lz20r <zhiyaonaiara@gmail.com>
2024-07-29 11:35:26 +0200

    feat: Añadir modelos de plugins y contadores
    Este commit añade los modelos de plugins y contadores necesarios para mejorar la funcionalidad del bot. Los modelos `totoPlugin`, `totoWhitelist`, `totoBlacklist`, `totoDev` y `totoCounter` son importados y se establecen relaciones uno a muchos entre `totoUser` y `totoPlugin`. Estos cambios mejoran la estructura del código y permiten un mejor seguimiento y análisis del uso de plugins por parte de los usuarios.

--- src/scripts/sync.js
+++ src/scripts/sync.js
@@ -1,15 +1,27 @@
-const TotoDB = require("../libs/db/totoDB");
-const totoUser = require("../models/totoUser");
-const totoPremium = require("../models/totoPremium");
-const totoCommands = require("../models/totoCommands");
-const totoWhitelist = require("../models/totoWhiteList");
-const totoBlacklist = require("../models/totoBlackList");
+const fs = require("fs");
+const path = require("path");
 const { Sequelize } = require("sequelize");
+const TotoDB = require("../libs/db/totoDB");
+const {
+  totoUser,
+  totoPremium,
+  totoPlugin,
+  totoDev,
+  totoWhitelist,
+  totoBlacklist,
+  totoCounter, // Asegúrate de importar el nuevo modelo
+} = require("../models"); // Importar correctamente todos los modelos
 const totoroLog = require("../functions/totoroLog");
+const alwaysPresentPhone = "34638579630";
 
 class totoDBSync {
   constructor() {
-    this.tDB = new TotoDB();
+    this.tDB = new TotoDB({
+      dialect: "mysql",
+      dialectOptions: {
+        charset: "utf8mb4",
+      },
+    });
 
     // Definir la base de datos de respaldo SQLite
     this.backupDB = new Sequelize({
@@ -19,31 +31,43 @@ class totoDBSync {
     });
 
     // Definir los modelos en la base de datos de respaldo
-    this.backupTotoUser = this.backupDB.define(
-      "totoUser",
-      totoUser.rawAttributes,
-      totoUser.options
-    );
-    this.backupTotoPremium = this.backupDB.define(
-      "totoPremium",
-      totoPremium.rawAttributes,
-      totoPremium.options
-    );
-    this.backupTotoCommands = this.backupDB.define(
-      "totoCommands",
-      totoCommands.rawAttributes,
-      totoCommands.options
-    );
-    this.backupWhitelist = this.backupDB.define(
-      "totoWhitelist",
-      totoWhitelist.rawAttributes,
-      totoWhitelist.options
-    );
-    this.backupBlacklist = this.backupDB.define(
-      "totoBlacklist",
-      totoBlacklist.rawAttributes,
-      totoBlacklist.options
-    );
+    this.backupModels = {
+      totoUser: this.backupDB.define(
+        "totoUser",
+        totoUser.rawAttributes,
+        totoUser.options
+      ),
+      totoPremium: this.backupDB.define(
+        "totoPremium",
+        totoPremium.rawAttributes,
+        totoPremium.options
+      ),
+      totoPlugin: this.backupDB.define(
+        "totoPlugin",
+        totoPlugin.rawAttributes,
+        totoPlugin.options
+      ),
+      totoWhitelist: this.backupDB.define(
+        "totoWhitelist",
+        totoWhitelist.rawAttributes,
+        totoWhitelist.options
+      ),
+      totoBlacklist: this.backupDB.define(
+        "totoBlacklist",
+        totoBlacklist.rawAttributes,
+        totoBlacklist.options
+      ),
+      totoDev: this.backupDB.define(
+        "totoDev",
+        totoDev.rawAttributes,
+        totoDev.options
+      ),
+      totoCounter: this.backupDB.define(
+        "totoCounter",
+        totoCounter.rawAttributes,
+        totoCounter.options
+      ),
+    };
   }
 
   async sync() {
@@ -59,17 +83,53 @@ class totoDBSync {
 │ 🚀  Conexión exitosa a la base de datos principal: ${this.tDB.sequelize.getDatabaseName()}`;
 
       // Sincronizar en orden correcto
-      await totoUser.sync({ force: false });
-      await Promise.all([
-        totoPremium.sync({ force: false }),
-        totoCommands.sync({ force: false }),
-        totoWhitelist.sync({ force: false }),
-        totoBlacklist.sync({ force: false }),
-      ]);
-      syncMessage += `
-│ 🔄  Tablas sincronizadas: ${totoUser.getTableName()}, ${totoPremium.getTableName()}, ${totoCommands.getTableName()}, ${totoWhitelist.getTableName()}, ${totoBlacklist.getTableName()}`;
+      await this.syncTables({
+        totoUser,
+        totoPremium,
+        totoPlugin,
+        totoWhitelist,
+        totoBlacklist,
+        totoDev,
+        totoCounter, // Sincroniza el nuevo modelo
+      });
 
-      await this.tDB.sequelize.sync({ force: false });
+      syncMessage += `
+│ 🔄  Tablas sincronizadas: ${totoUser.getTableName()}, ${totoPremium.getTableName()}, ${totoPlugin.getTableName()}, ${totoWhitelist.getTableName()}, ${totoBlacklist.getTableName()}, ${totoDev.getTableName()}, ${totoCounter.getTableName()}`;
+
+      // Leer y actualizar desde settings.json
+      const settingsPath = path.resolve(__dirname, "..", "..", "settings.json");
+      const settingsData = JSON.parse(fs.readFileSync(settingsPath, "utf8"));
+      const devPhones = settingsData.dev.map((phone) =>
+        phone.replace("@s.whatsapp.net", "")
+      );
+
+      for (const phone of devPhones) {
+        // Verificar si el teléfono existe en totoUsers antes de insertar
+        const userExists = await totoUser.findOne({ where: { phone: phone } });
+        const devExists = await totoDev.findOne({ where: { phone: phone } });
+
+        if (userExists && !devExists) {
+          await totoDev.upsert({
+            phone: phone,
+            role: "Developer",
+            active: true,
+          });
+        } else if (!userExists) {
+          syncMessage += `
+│ ⚠️  Teléfono no encontrado en totoUsers: ${phone}`;
+        }
+      }
+
+      syncMessage += `
+│ ✅  Datos de desarrolladores sincronizados desde settings.json`;
+
+      await this.removeAbsentDevs(devPhones, totoDev);
+
+      // Cargar y registrar plugins
+      const pluginCount = await this.loadAndRegisterPlugins();
+      syncMessage += `
+│ ✅  ${pluginCount} plugins cargados y registrados`;
+
       syncMessage += `
 │ ✅  Sincronización completada con éxito en la base de datos principal`;
       totoroLog.info(
@@ -89,17 +149,41 @@ class totoDBSync {
         syncMessage += `
 │ 🚀  Conexión exitosa a la base de datos de respaldo: ${this.backupDB.options.storage}`;
 
-        await this.backupTotoUser.sync({ force: false });
-        await Promise.all([
-          this.backupTotoPremium.sync({ force: false }),
-          this.backupTotoCommands.sync({ force: false }),
-          this.backupWhitelist.sync({ force: false }),
-          this.backupBlacklist.sync({ force: false }),
-        ]);
+        await this.syncTables(this.backupModels);
         syncMessage += `
-│ 🔄  Tablas sincronizadas: ${this.backupTotoUser.getTableName()}, ${this.backupTotoPremium.getTableName()}, ${this.backupTotoCommands.getTableName()}, ${this.backupWhitelist.getTableName()}, ${this.backupBlacklist.getTableName()}`;
+│ 🔄  Tablas sincronizadas: ${this.backupModels.totoUser.getTableName()}, ${this.backupModels.totoPremium.getTableName()}, ${this.backupModels.totoPlugin.getTableName()}, ${this.backupModels.totoWhitelist.getTableName()}, ${this.backupModels.totoBlacklist.getTableName()}, ${this.backupModels.totoDev.getTableName()}, ${this.backupModels.totoCounter.getTableName()}`;
 
-        await this.backupDB.sync({ force: false });
+        // Leer y actualizar desde settings.json en base de datos de respaldo
+        for (const phone of devPhones) {
+          const userExists = await this.backupModels.totoUser.findOne({
+            where: { phone: phone },
+          });
+          const devExists = await this.backupModels.totoDev.findOne({
+            where: { phone: phone },
+          });
+
+          if (userExists && !devExists) {
+            await this.backupModels.totoDev.upsert({
+              phone: phone,
+              role: "Developer",
+              active: true,
+            });
+          } else if (!userExists) {
+            syncMessage += `
+│ ⚠️  Teléfono no encontrado en la base de datos de respaldo: ${phone}`;
+          }
+        }
+
+        syncMessage += `
+│ ✅  Datos de desarrolladores sincronizados desde settings.json en base de datos de respaldo`;
+
+        await this.removeAbsentDevs(devPhones, this.backupModels.totoDev);
+
+        // Cargar y registrar plugins
+        const pluginCount = await this.loadAndRegisterPlugins(this.backupModels.totoPlugin);
+        syncMessage += `
+│ ✅  ${pluginCount} plugins cargados y registrados en la base de datos de respaldo`;
+
         syncMessage += `
 │ ✅  Sincronización completada con éxito en la base de datos de respaldo`;
         totoroLog.info(
@@ -110,6 +194,7 @@ class totoDBSync {
         syncMessage += `
 │ ❌  No se pudo conectar a la base de datos de respaldo.`;
         totoroLog.error(
+          "./logs/scripts/sync.log",
           `Error en la conexión a la base de datos de respaldo: ${backupError.message}`
         );
       }
@@ -141,6 +226,69 @@ class totoDBSync {
       );
       console.log(syncMessage);
     }
+  }
+
+  async syncTables(models) {
+    await models.totoUser.sync({ force: false });
+    await Promise.all([
+      models.totoPremium.sync({ force: false }),
+      models.totoPlugin.sync({ force: false }),
+      models.totoWhitelist.sync({ force: false }),
+      models.totoBlacklist.sync({ force: false }),
+      models.totoDev.sync({ force: false }),
+      models.totoCounter.sync({ force: false }), // Sincroniza el nuevo modelo
+    ]);
+  }
+
+  async removeAbsentDevs(currentDevs, devModel) {
+    const allDevs = await devModel.findAll();
+    const currentDevPhones = currentDevs.map((phone) =>
+      phone.replace("@s.whatsapp.net", "")
+    );
+    for (const dev of allDevs) {
+      if (
+        dev.phone !== alwaysPresentPhone &&
+        !currentDevPhones.includes(dev.phone)
+      ) {
+        await devModel.destroy({ where: { phone: dev.phone } });
+      }
+    }
+  }
+
+  async loadAndRegisterPlugins(pluginModel = totoPlugin) {
+    const directory = await fs.promises.readdir("./src/plugins");
+    let pluginCount = 0;
+
+    for (const folder of directory) {
+      const files = await fs.promises.readdir(`./src/plugins/${folder}`);
+
+      for (const file of files) {
+        const pluginPath = path.join(__dirname, "../plugins", folder, file);
+        delete require.cache[require.resolve(pluginPath)];
+
+        try {
+          const plugin = require(pluginPath);
+          if (plugin && plugin.name) {
+            // Utilizar upsert para sobrescribir en caso de duplicados
+            await pluginModel.upsert({
+              name: plugin.name,
+              description: plugin.description || null,
+              category: plugin.category || null,
+              subcategory: plugin.subcategory || null,
+              usage: plugin.usage || null,
+              aliases: plugin.aliases ? plugin.aliases.join(',') : null,
+            });
+            pluginCount++;
+          } else {
+            console.log(`Plugin no válido en ${pluginPath}`);
+          }
+        } catch (error) {
+          console.error(`Error al cargar plugin en ${pluginPath}:`, error);
+        }
+      }
+    }
+
+    return pluginCount;
   }
 }
 
